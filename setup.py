787a307fccf8d958a040aad1b1bbae6d7b3d29cf
Brendan Moore <brendan@moore-email.com>
2018-12-09 21:39:03 -0500

    Another Typo in setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent"],
     long_description=long_description,
     long_description_content_type="text/markdown",
